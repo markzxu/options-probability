793cc7f3634b624aa3dd14f03177ee8c982edf72
Mark Xu <mzxu@caltech.edu>
2020-07-25 15:58:14 -0700

    added requirements.txt and some experimental dash development

---
+++ app.py
@@ -0,0 +1,80 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Input
+import pandas as pd
+import plotly.graph_objs as go
+
+app = dash.Dash()
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montreal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC'
+    ),
+    dcc.Dropdown(
+        id='demo-dropdown2',
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montreal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC'
+    ),
+    dcc.Input(
+            id="input",
+            type="text",
+            placeholder="APPL",
+    ),
+    html.Button('Click Me', id='button'),
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.8,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    ),
+    html.Div(id='output-container'),
+])
+
+@app.callback(Output('output-container', 'children'),
+              [Input('button', 'n_clicks')]
+)
+def update(text):
+    print(dash.callback_context.triggered)
+    print(text)
+    return text
+
+if __name__ == '__main__':
+    app.run_server()
