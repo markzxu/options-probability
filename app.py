1228ee35bfaefc6e208f0d3ab8c6e09b32975613
Mark Xu <mzxu@caltech.edu>
2020-07-25 20:57:21 -0700

    something's wrong! But it _sort of_ works???

--- app.py
+++ app.py
@@ -7,39 +7,33 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+from yahoo import cdf, get_dates
 
 EPSILON = 1e-3
 app = dash.Dash()
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    '5d1ea79569ed194d432e56108a04d188/raw/' +
-    'a9f9e8076b837d541398e999dcbac2b2826a81f8/' +
-    'gdp-life-exp-2007.csv')
-
-
-def get_dates():
-    return ['2003-02-19', '2003-02-20']
-
-
-def get_distribution(date, ticker):
-    return np.histogram(np.random.normal(0, 1, 1000), bins=100, normed=True)
-
-
 app.layout = html.Div([
-    dcc.Dropdown(
-        id='date-dropdown',
-        options=[{'label': d, 'value': d} for d in get_dates()],
-        value=get_dates()[0]
-    ),
     dcc.Input(
         id="ticker-input",
         type="text",
         placeholder="Ticker Symbol",
     ),
+    dcc.Dropdown(
+        id='date-dropdown',
+        options=[{'label': d, 'value': d} for d in get_dates("AAPL")],
+        value=get_dates("AAPL")[0]
+    ),
+
     html.Button('Get Distribution', id='button'),
     dcc.Graph('probability-dist')
 ])
+
+@app.callback(Output('date-dropdown', 'options'),
+              [Input('ticker-input', 'value')])
+def update_dropdown(ticker):
+    dates = get_dates(ticker)
+    options = [{'label': i, 'value': i} for i in dates]
+    return options
 
 
 @app.callback(Output('probability-dist', 'figure'),
@@ -48,8 +42,13 @@ app.layout = html.Div([
                      State('ticker-input', 'value')]
               )
 def update(n_clicks, date, ticker):
-    values, bins = get_distribution(date, ticker)
-    data = [go.Bar(x=bins, y=values, width=bins[1]-bins[0]+EPSILON)]
+    if date is None:
+        return None
+    data = cdf(ticker, date)
+    print(data)
+    widths = data['range_start'].diff().tolist()
+    widths[0] = widths[1]
+    data = [go.Bar(x=data['range_start'], y=data['prob']/np.array(widths), width=widths)]
     fig = go.Figure(data=data)
     return fig
 
